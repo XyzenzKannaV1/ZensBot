50a6b9faf9e4da9455147043c3d70d35fa51c0db
Kanna <112399198+XyzenzKannaV1@users.noreply.github.com>
2022-10-16 12:16:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,11 +16,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['62895604670507']
-global.ownernomer = "62895604670507"
-global.premium = ['62895604670507','6281328144300']
-global.packname = 'Zenss'
-global.author = 'Cuyy'
+global.owner = ['6288980870067']
+global.ownernomer = "6288980870067"
+global.premium = ['6288980870067','6283112831554']
+global.packname = 'Swift Kawai'
+global.author = 'Siesta - Multi Device'
 global.sessionName = 'zenss'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
